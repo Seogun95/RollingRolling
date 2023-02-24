6d4cda54e187f2f1cf983b3c47e29c8784475e19
dajeong <daaa_j@naver.com>
2023-02-24 23:25:55 +0900

    design: edit components backup

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,31 +1,30 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 export default function Sidebar() {
-  return (
-    <div>
-      <LayoutSidebar>
-        <ProfileBox></ProfileBox>
-      </LayoutSidebar>
-    </div>
-  );
+    return (
+        <div>
+            <LayoutSidebar>
+                <ProfileBox></ProfileBox>
+            </LayoutSidebar>
+        </div>
+    );
 }
 
 const ProfileBox = styled.div`
-  ${(props) => props.theme.FlexRow};
-  margin: auto;
-  width: 100%;
-  height: 100%;
-
-  background-color: white;
+    ${(props) => props.theme.FlexRow};
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    background-color: white;
 `;
 
 const LayoutSidebar = styled.div`
-  background-color: ${(props) => props.theme.CL.brandColor};
-  overflow: hidden;
-  width: 26vw;
-  min-height: 90vh;
-  border-radius: 30px;
-  margin-right: 10px;
-  box-shadow: 3px 0px 0px 3px ${(props) => props.theme.CL.scroll};
+    background-color: ${(props) => props.theme.CL.brandColor};
+    overflow: hidden;
+    width: 26vw;
+    min-height: 90vh;
+    border-radius: 30px;
+    margin-right: 10px;
+    box-shadow: 3px 0px 0px 3px ${(props) => props.theme.CL.scroll};
 `;

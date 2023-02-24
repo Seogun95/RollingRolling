9dbbf89eabe4aa5da9b3a9ccaf2e421f6942cbf7
9mean2 <o3omoomin@gmail.com>
2023-02-24 21:41:32 +0900

    design: Sidebar 컴포넌트로 분리 , DetailPage css 작업중

---
+++ src/components/Layout/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Sidebar() {
+  return (
+    <div>
+      <LayoutSidebar>
+        <ProfileBox></ProfileBox>
+      </LayoutSidebar>
+    </div>
+  );
+}
+
+const ProfileBox = styled.div`
+  ${(props) => props.theme.FlexRow};
+  margin: auto;
+  width: 100%;
+  height: 100%;
+
+  background-color: white;
+`;
+
+const LayoutSidebar = styled.div`
+  background-color: ${(props) => props.theme.CL.brandColor};
+  overflow: hidden;
+  width: 26vw;
+  min-height: 90vh;
+  border-radius: 30px;
+  margin-right: 10px;
+  box-shadow: 3px 0px 0px 3px ${(props) => props.theme.CL.scroll};
+`;

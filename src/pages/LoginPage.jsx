68fc667a4652f7e14aa9a1095c4a5d0635712807
Seogun95 <ksj083895@gmail.com>
2023-02-26 04:05:44 +0900

    refactor: 페이지 state 훅을 통한 리펙터링

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,50 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { FaUserAlt, FaLock } from 'react-icons/fa';
 import Input from '../components/elements/Input.jsx';
 import Button from '../components/elements/Button';
 import { useNavigate } from 'react-router-dom';
+import useLoginInput from '../hooks/useLoginInput.jsx';
 
 function LoginPage() {
-  const [inputId, setinputId] = useState('');
-  const [inputPw, setinputPw] = useState('');
-
-  const [idMessage, setIdMessage] = useState('아이디를 입력해주세요');
-  const [pwMessage, setPwMessage] = useState('비밀번호를 입력해주세요');
-
-  const [isId, setIsId] = useState(false);
-  const [isPw, setIsPw] = useState(false);
-  // const getToken = Cookies.get('token');
   const navigate = useNavigate();
+  const idRegex = /^(?=.*?[0-9])(?=.*?[a-z])[a-zA-Z0-9]{4,10}$/;
+  const [inputId, inputIdHandler, alertId, checkIdRegex] = useLoginInput(
+    '',
+    '',
+    '아이디는 영어 소문자, 숫자 조합의 4-10자의 형식으로 입력해주세요.',
+    '사용 가능한 아이디 입니다.',
+    idRegex
+  );
 
-  // id input change
-  const idChangeHanlder = (e) => {
-    setinputId(e.target.value);
-    const idRegex = /^(?=.*?[0-9])(?=.*?[a-z]).{5,}$/;
-    if (!idRegex.test(e.target.value)) {
-      setIdMessage(
-        '영어 소문자, 숫자 조합의 5자 이상의 아이디만 작성할 수 있습니다.'
-      );
-      setIsId(false);
-    } else {
-      setIdMessage('');
-      setIsId(true);
-    }
-  };
-  // pw input change
-  const pwChangeHandler = (e) => {
-    setinputPw(e.target.value);
-    const pwRegex = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
-    if (!pwRegex.test(e.target.value)) {
-      setPwMessage(
-        '영어 소문자와 숫자, 특수문자 조합의 8-20자의 비밀번호만 작성할 수 있습니다.'
-      );
-      setIsPw(false);
-    } else {
-      setPwMessage('');
-      setIsPw(true);
-    }
-  };
+  const pwRegex = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,15}$/;
+  const [inputPw, inputPwHandler, alertPw, checkPwRegex] = useLoginInput(
+    '',
+    '비밀번호를 입력해주세요.',
+    '영문과 숫자, 특수문자 조합의 8-20자의 비밀번호를 사용해야 합니다.',
+    '사용 가능한 비밀번호 입니다.',
+    pwRegex
+  );
 
   //스크롤 방지
   useEffect(() => {
@@ -66,22 +46,26 @@ function LoginPage() {
           <Input
             text={'아이디'}
             value={inputId}
-            onChange={idChangeHanlder}
+            onChange={inputIdHandler}
             type={'id'}
+            len={'10'}
           >
             <FaUserAlt />
           </Input>
-          <LoginAlertSpan isIdOrPw={isId}>{idMessage}</LoginAlertSpan>
+          <LoginAlertSpan height={'25px'} isIdOrPw={checkIdRegex}>
+            {alertId}
+          </LoginAlertSpan>
           <Input
             text={'비밀번호'}
             value={inputPw}
-            onChange={pwChangeHandler}
+            onChange={inputPwHandler}
             type={'password'}
+            len={'15'}
           >
             <FaLock />
           </Input>
-          <LoginAlertSpan isIdOrPw={isPw} height={'50px'}>
-            {pwMessage}
+          <LoginAlertSpan isIdOrPw={checkPwRegex} height={'50px'}>
+            {alertPw}
           </LoginAlertSpan>
           <Button bg={'#8CB46D'} h={'3.125rem'} size={'0.9rem'}>
             로그인
@@ -92,7 +76,7 @@ function LoginPage() {
           <span>RollingRolling 회원이 아니신가요? </span>
           <Button
             onClick={moveRegistrationPg}
-            color={'white'}
+            color={'black'}
             size={'0.9rem'}
             w={'auto'}
           >
@@ -111,21 +95,21 @@ export const LoginModalWrapper = styled.div`
   position: fixed;
   inset: 0;
   z-index: 100;
+  background: url('https://i.imgur.com/GFBmTcM.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const LoginText = styled.h1`
   font-size: ${(props) => props.theme.FS.xl};
-  color: white;
 `;
 
 export const LoginModal = styled.div`
   display: flex;
   justify-content: flex-start;
   flex-direction: column;
-
-  background: #161616aa;
+  background: #ffffff76;
   backdrop-filter: blur(3px);
-
   padding: 3rem;
   border-radius: 1rem;
   width: 500px;
@@ -134,9 +118,9 @@ export const LoginModal = styled.div`
 
 export const LoginAlertSpan = styled.div`
   font-size: 0.8rem;
-  padding: 0.2rem 0.5rem 0.5rem;
+  padding: 0.4rem 0.5rem 0.5rem;
   height: ${(props) => props.height};
-  color: ${(props) => (props.isIdOrPw ? 'springgreen' : 'tomato')};
+  color: ${(props) => (props.isIdOrPw ? '#58793e' : 'tomato')};
 `;
 
 export const LoginInputContainer = styled.form`
@@ -150,7 +134,7 @@ export const LoginGoToSignup = styled.div`
   justify-content: flex-end;
   span {
     font-size: ${(props) => props.theme.FS.xs};
-    color: white;
+    color: #000000;
     opacity: 0.6;
   }
 `;

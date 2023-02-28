716f3f1e043a96ebdb78a4798290af2e469035e6
Seogun95 <ksj083895@gmail.com>
2023-03-01 08:30:22 +0900

    remove: 스토어 삭제

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/config/configStore';
 import { GlobalStyle } from './style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <>
     <GlobalStyle />
     <App />
-  </Provider>
+  </>
 );

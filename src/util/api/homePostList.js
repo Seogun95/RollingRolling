785584469a9551610f224e0333f8067d2cd540d5
dajeong <daaa_j@naver.com>
2023-03-02 04:57:07 +0900

    feat: 기능 수정

--- src/util/api/homePostList.js
+++ src/util/api/homePostList.js
@@ -1,9 +1,23 @@
+import { async } from 'q';
 import { api } from './api';
 
 // homepage 전체 postlist 조회
 export const getHomePostList = async (data) => {
-  console.log('gethomepost : ', data);
+  //console.log('gethomepost : ', data);
   const response = await api.get(`/api/home/${data}`);
-  console.log('gethomepost response: ', response);
+  //console.log('gethomepost response: ', response);
+  return response.data;
+};
+
+export const searchUser = async (data) => {
+  console.log(typeof data.search);
+  const input = 'aaa123';
+
+  const encodedInput = encodeURIComponent(input);
+  const response = await api.get(`/api/user/search/aaa123`, {
+    headers: {
+      Authorization: data.token,
+    },
+  });
   return response.data;
 };
